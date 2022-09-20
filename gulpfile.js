b96a092c75fc8b561df595625f112b1a2e9d74c2
Al Tsaplin <al.tsaplin@gmail.com>
2022-09-20 11:20:25 +0300

    done: section.PLAN-AND-MANAGE

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,30 @@
 const { src, dest, watch, parallel, series } = require('gulp');
 
-const scss         = require('gulp-sass');
-const concat       = require('gulp-concat');
-const browserSync  = require('browser-sync').create();
-const uglify       = require('gulp-uglify-es').default;
+const scss = require('gulp-sass');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+const uglify = require('gulp-uglify-es').default;
 const autoprefixer = require('gulp-autoprefixer');
-const imagemin     = require('gulp-imagemin');
-const del          = require('del');
+const imagemin = require('gulp-imagemin');
+const del = require('del');
 
 function browsersync() {
   browserSync.init({
     server: {
-      baseDir: 'app/'
-    }
+      baseDir: 'app/',
+    },
+    notify: false,
   });
 }
 
 function cleanDist() {
-  return del('dist')
+  return del('dist');
 }
 
 function images() {
-  return src("app/images/**/*")
-    .pipe(imagemin(
-      [
+  return src('app/images/**/*')
+    .pipe(
+      imagemin([
         imagemin.gifsicle({ interlaced: true }),
         imagemin.mozjpeg({ quality: 75, progressive: true }),
         imagemin.optipng({ optimizationLevel: 5 }),
@@ -32,44 +33,45 @@ function images() {
         }),
       ])
     )
-    .pipe(dest("dist/images"));
+    .pipe(dest('dist/images'));
 }
 // here I'm adding additional downloaded libraries into src([''])
 function scripts() {
-  return src([
-    'node_modules/jquery/dist/jquery.js',
-    'app/js/main.js'
-  ])
-  .pipe(concat('main.min.js'))
-  .pipe(uglify())
-  .pipe(dest('app/js'))
+  return src(['node_modules/jquery/dist/jquery.js', 'app/js/main.js'])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .pipe(dest('app/js'));
 }
 
 function styles() {
-  return src("app/scss/style.scss")
-    .pipe(scss({ outputStyle: "compressed" }))
-    .pipe(concat("style.min.css"))
-    .pipe(autoprefixer({
-      overrideBrowserslist: ['last 10 version'],
-      grid: true
-    }))
-    .pipe(dest("app/css"))
+  return src('app/scss/style.scss')
+    .pipe(scss({ outputStyle: 'compressed' }))
+    .pipe(concat('style.min.css'))
+    .pipe(
+      autoprefixer({
+        overrideBrowserslist: ['last 10 version'],
+        grid: true,
+      })
+    )
+    .pipe(dest('app/css'))
     .pipe(browserSync.stream());
 }
 
 function build() {
-  return src([
-    'app/css/style.min.css',
-    'app/fonts/**/*',
-    'app/js/main.min.js',
-    'app/*.html'
-  ], {base: 'app'})
-  .pipe(dest('dist'))
+  return src(
+    [
+      'app/css/style.min.css',
+      'app/fonts/**/*',
+      'app/js/main.min.js',
+      'app/*.html',
+    ],
+    { base: 'app' }
+  ).pipe(dest('dist'));
 }
 
 function watching() {
   watch(['app/scss/**/*.scss'], styles);
-  watch(['app/js/**/*.js' ,'!app/js/main.min.js'], scripts); //"!" means excluded, otherwise it be looped when watching is active
+  watch(['app/js/**/*.js', '!app/js/main.min.js'], scripts); //"!" means excluded, otherwise it be looped when watching is active
   watch(['app/*.html']).on('change', browserSync.reload);
 }
 
