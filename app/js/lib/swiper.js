48204983b08bbbb25aecb8f15ebb231c96f01124
Al Zaplin <zaplin.dev@gmail.com>
2025-12-28 14:59:55 +0300

    Feat: (Slider 'Swiper' for section 'blog') Setup slider

--- app/js/lib/swiper.js
+++ app/js/lib/swiper.js
@@ -6,11 +6,33 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 
 export default function swiper() {
-  const swiper = new Swiper(".section-hero-image", {
+  new Swiper(".section-hero-image", {
+    modules: [Pagination],
+    pagination: {
+      el: ".slider-hero-image__pagination",
+      clickable: true,
+    },
+  });
+
+  new Swiper(".slider-blog-container", {
+    modules: [Navigation, Pagination],
+    navigation: {
+      nextEl: ".btn-blog--next",
+      prevEl: ".btn-blog--prev",
+    },
+    pagination: {
+      el: ".slider-blog__pagination",
+      clickable: true,
+    },
+  });
+}
+
+/* export default function swiper() {
+  const swiper = new Swiper(".slider-blog", {
     modules: [Navigation, Pagination],
     pagination: {
       el: ".slider-hero-image__pagination",
       clickable: true,
     },
   });
-}
+} */
